f6f7637ecb6ed353b9ce0c7e4e5672aaca254f61
Cédric LESUEUR <cedric1lesueur@gmail.com>
2024-02-23 20:40:41 +0100

    updated ship movement

--- Assets/Scripts/BoatController.cs
+++ Assets/Scripts/BoatController.cs
@@ -1,32 +1,37 @@
-using System;
 using UnityEngine;
 
 public class BoatController : MonoBehaviour
 {
     public float speed = 5f; // Adjust this to set the speed of the boat
-    public float rotationSpeed = 100f; // Adjust this to set the rotation speed of the boat
+    public float maxRotSpeed = 100f; // Adjust this to set the rotation speed of the boat
+    public float thresholdMaxRotSpeed = 1.0f;
 
-    private Rigidbody2D rb;
+    private float _currentRotSpeed = 0f;
+    private Rigidbody2D _rb;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
-        // Get input axes
+        /// Get input axes
         float verticalInput = Input.GetAxis("Vertical");
         float horizontalInput = Input.GetAxis("Horizontal");
 
         // Apply forward or backward force based on vertical input
-        rb.AddForce(transform.up * verticalInput * speed);
+        _rb.AddForce(transform.up * verticalInput * speed);
 
-        Vector2 vec = Vector2.Reflect(rb.velocity * -1, transform.up) * 0.02f;
-        rb.velocity += vec;
+        Vector2 vec = Vector2.Reflect(_rb.velocity * -1, transform.up) * 0.02f;
+        _rb.velocity += vec;
+
+        Debug.Log(_rb.velocity.magnitude);
+
+        _currentRotSpeed = Mathf.Clamp01(_rb.velocity.magnitude / thresholdMaxRotSpeed) * maxRotSpeed;
 
         // Apply rotation based on horizontal input
-        float rotation = -horizontalInput * rotationSpeed * Time.fixedDeltaTime;
-        rb.MoveRotation(rb.rotation + rotation);
+        float rotation = -horizontalInput * _currentRotSpeed * Time.fixedDeltaTime;
+        _rb.MoveRotation(_rb.rotation + rotation);
     }
 }
