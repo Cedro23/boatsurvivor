77201352c5ef8f14e3704cd1daded4a0070a55bb
Cédric LESUEUR <cedric1lesueur@gmail.com>
2024-02-24 11:22:45 +0100

    Improved boat controller

--- Assets/Scripts/BoatController.cs
+++ Assets/Scripts/BoatController.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class BoatController : MonoBehaviour
 {
-    public float speed = 5f; // Adjust this to set the speed of the boat
-    public float maxRotSpeed = 100f; // Adjust this to set the rotation speed of the boat
+    [Header("Statistics")]
+    public float acceleration = 5f;
+    public float topForwardSpeed = 10f;
+    public float reverseAcceleration = 1.0f;
+    public float topReverseSpeed = 4f;
+    public float maxRotSpeed = 100f;
     public float thresholdMaxRotSpeed = 1.0f;
 
-    private float _currentRotSpeed = 0f;
     private Rigidbody2D _rb;
 
     void Start()
@@ -16,22 +19,33 @@ public class BoatController : MonoBehaviour
 
     void FixedUpdate()
     {
-        /// Get input axes
+        // Get input axes
         float verticalInput = Input.GetAxis("Vertical");
         float horizontalInput = Input.GetAxis("Horizontal");
-
-        // Apply forward or backward force based on vertical input
-        _rb.AddForce(transform.up * verticalInput * speed);
-
-        Vector2 vec = Vector2.Reflect(_rb.velocity * -1, transform.up) * 0.02f;
-        _rb.velocity += vec;
-
+        
+        // Debug
         Debug.Log(_rb.velocity.magnitude);
 
-        _currentRotSpeed = Mathf.Clamp01(_rb.velocity.magnitude / thresholdMaxRotSpeed) * maxRotSpeed;
+        // Acceleration
+        bool isForward = verticalInput > 0;
+        if (isForward)
+        {
+            if (_rb.velocity.magnitude < topForwardSpeed)
+                _rb.AddForce(acceleration * verticalInput * transform.up);
+        }
+        else
+        {
+            if(_rb.velocity.magnitude < topReverseSpeed)
+                _rb.AddForce(reverseAcceleration * verticalInput * transform.up);
+        }
 
-        // Apply rotation based on horizontal input
-        float rotation = -horizontalInput * _currentRotSpeed * Time.fixedDeltaTime;
-        _rb.MoveRotation(_rb.rotation + rotation);
+            // Water resistance
+            Vector2 vec = Vector2.Reflect(_rb.velocity * -1, transform.up) * 0.02f;
+        _rb.velocity += vec;
+
+        // Rotation
+        float rotationSpeed = Mathf.Clamp01(_rb.velocity.magnitude / thresholdMaxRotSpeed) * maxRotSpeed;
+        float rotation = -horizontalInput * rotationSpeed * Time.fixedDeltaTime;
+        transform.Rotate(0, 0, rotation);
     }
 }
